22748606a9411e47faabd3306017ec0bbcf5234b
sazib <sazibislam1@gmail.com>
2024-12-02 16:48:14 +0600

    README.md

--- app/src/main/java/com/example/tenmin/data/remote/source/AuthRemote.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tenmin.data.remote.source
-
-interface AuthRemote {
-    // suspend fun login(request: LoginRequestDto): ResponseResource<LoginResponseDto>
-    // suspend fun signup(request: SignupRequestDto): ResponseResource<SignupResponseDto>
-}
